2a9ab606d0bcec8a89f38b20d26347b0b0c5e757
dkingstong <d.kingston12@gmail.com>
2024-01-25 09:14:21 -0600

    Send key value correctly to kafka

--- go/server.go
+++ go/server.go
@@ -2,12 +2,13 @@ package main
 
 import (
     "fmt"
+		"io/ioutil"
+		"encoding/json"
     "log"
     "net/http"
 		"strconv"
 		"sync"
 		"github.com/confluentinc/confluent-kafka-go/kafka" //the confluent library to work with kafka
-		"encoding/json"
 )
 
 var counter int
@@ -25,20 +26,29 @@ func incrementCounter(w http.ResponseWriter, r *http.Request) {
 	mutex.Unlock()
 }
 
+// it's important to have this in Uppercase because this means the keys are exported which means they're available to
+// the external library that parses into a JSON
 type Message struct {
-	id int
-	message string
+	Id int 					`json:"id"`
+	Message string	`json:"message"`
 }
 
 func sendMessage(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case "POST":
-		var jsonMessage Message
-		// Try to decode the request body into the struct. If there is an error,
-    // respond to the client with the error message and a 400 status code.
-    err := json.NewDecoder(r.Body).Decode(&jsonMessage)
+		// Read the request body
+		body, err := ioutil.ReadAll(r.Body)
 		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
+			http.Error(w, "Error reading request body", http.StatusInternalServerError)
+			return
+		}
+
+		// Unmarshal the JSON body into the struct
+		var jsonMessage Message
+		err = json.Unmarshal(body, &jsonMessage)
+
+		if err != nil {
+			http.Error(w, "Error unmarshalling JSON", http.StatusBadRequest)
 			return
 		}
 
@@ -55,15 +65,18 @@ func sendMessage(w http.ResponseWriter, r *http.Request) {
 			fmt.Printf("Error creating Kafka producer: %v\n", err)
 			return
 		}
-		//defer producer.Close()
 
 		// Define a Kafka topic to produce messages to
     topic := "example"
 
+		key := jsonMessage.Id
+		value := jsonMessage.Message
+
     // Produce a sample message
     message := &kafka.Message{
         TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
-        Value:          []byte(jsonMessage.message),
+				Key:            []byte(strconv.FormatInt(int64(key), 10)),
+        Value:          []byte(value),
     }
 
 		// Produce the message to the Kafka topic
@@ -74,6 +87,8 @@ func sendMessage(w http.ResponseWriter, r *http.Request) {
 		}
 		// Wait for any outstanding messages to be delivered and delivery reports to be received
     producer.Flush(15 * 1000) // 15 seconds timeout
+
+		producer.Close()
 		
 		fmt.Fprintf(w, "Message sent successfuly")
 		return
